b1c2a3948026d368f635decc1cd51b33b94adecd
iukeke <iukeke@atguigu.com>
2021-06-02 18:56:20 +0800

    hot-fix  commit

--- src/test/Test.java
+++ src/test/Test.java
@@ -18,6 +18,7 @@ public class Test {
 
         //1.用户登录//1.用户登录//1.用户登录//1.用户登录//1.用户登录//1.用户登录
         System.out.println(1111);
+        System.out.println(1111);
         //1.用户登录
         /*Scanner sc = new Scanner(System.in);
         System.out.println("请输入用户名：");
