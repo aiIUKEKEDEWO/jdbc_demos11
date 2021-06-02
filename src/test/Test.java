d07fce3b25d44086231b667854d59b43c024ab3a
iukeke <iukeke@atguigu.com>
2021-06-02 17:45:48 +0800

    gitee commit

--- src/test/Test.java
+++ src/test/Test.java
@@ -14,6 +14,7 @@ import java.util.Scanner;
  */
 public class Test {
     public static void main(String[] args) {
+        System.out.println(1111);
         //1.用户登录
         /*Scanner sc = new Scanner(System.in);
         System.out.println("请输入用户名：");
